fa846e960fd05e80db7a310620f880ac4cb30e9e
Jane Justiz <janejustiz@gmail.com>
2018-06-14 03:10:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.each do |x|
+    array.delete(x)
+    array.unshift(badge_maker(x))
+  end
+  array.reverse
+end
+
+def assign_rooms(array)
+  number = 1
+  array.each do |name|
+    array.delete(name)
+    array.unshift("Hello, #{name}! You'll be assigned to room #{number}!")
+    number += 1
+  end
+  array.reverse
+end
+
+def printer
+  array.each do |x|
+    batch_badge_creator(x)
+    assign_rooms(x)
+end
